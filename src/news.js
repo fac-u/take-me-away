48b2aab66e4e336a74751d8251f3962bdce1d65f
lucymonie <lucy.monie.hall@gmail.com>
2016-11-23 11:06:08 +0000

    handle errors in news api

--- src/news.js
+++ src/news.js
@@ -3,12 +3,15 @@ const {newsApiKey} = require('../api-keys.js');
 
 var getArticles = (cb) => {
   request(`https://newsapi.org/v1/articles?source=the-guardian-uk&sortBy=latest&apiKey=${newsApiKey}`, (err, response, body) => {
-    if (err) cb(err);
-    else {
+    let articles;
+    if (err || response.statusCode !== 200) {
+      articles = [{title: 'Giraffes rob bank in Nairobi'}, {title: 'Aliens land on earth, not in America'}];
+    } else {
+      console.log('Body is: ', body)
       let result = JSON.parse(body);
-      let articles = result.articles.slice(0,2);
-      cb(null, articles);
+      articles = result.articles.slice(0,2);
     }
+    cb(articles);
   });
 }
 
